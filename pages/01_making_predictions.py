274022c0683ca1d173bf0c347acdaf00903eec12
zohndai <zohndai@gmail.com>
2024-08-26 23:06:11 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -4,7 +4,7 @@ import numpy as np
 import rdkit
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from rdkit.Chem import Draw
+#from rdkit.Chem import Draw
 import matplotlib.pyplot as plt
 
 
