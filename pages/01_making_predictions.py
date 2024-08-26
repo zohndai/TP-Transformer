c656e59a4359c84c3ea62acdc587ed8fdc09a354
zohndai <zohndai@gmail.com>
2024-08-26 23:47:27 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -22,3 +22,11 @@ st.write('You selected:', select)
 #select = st.radio("Please specify the property or activity you want to predict", ('OH radical', 'SO4- radical', 'Koc', 'Solubility','pKd','pIC50','CCSM_H','CCSM_Na', 'Lipo','FreeSolv' ))
 st.subheader('Please input the precursors of the ROSs')
 st.text_input("Please offer Chemical name, CAS number, or SMILES of precursors', e.g. 'OO' for H2O2", "OO")
+
+st.subheader('Please select the method for extertal energy input for the ROSs generation "UV")
+st.selection_box("UV", "heat", "Visible Light", "Micro Wave", "Electricity", "Ultrasound")
+
+st.subheader('Please input the reaction pH for pollutant degradation')
+st.text_input("7.00")
+
+
