e7c956bab84a4392f95d40c3b6a6996f616e1382
zohndai <zohndai@gmail.com>
2024-08-27 17:25:21 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -197,13 +197,12 @@ def main():
 	dp_smis = pd.read_csv(opt_tsl.output,header=None)
 	smis_li=["".join(dp_smi.split(" ")) for dp_smi in dp_smis[0]]
 	message_container = st.empty()
-	message_container.text(f"Prediction result:
-  top1:{smis_li[0]};/
-  top2:{smis_li[1]};/
-  top3:{smis_li[2]};/
-  top4:{smis_li[3]};/
-  top5:{smis_li[4]}"/
-			      )
+	message_container.text(f"Prediction result:")
+	message_container.text(f"top1:{smis_li[0]}")
+ 	message_container.text(f"top2:{smis_li[1]}")
+	message_container.text(f"top3:{smis_li[2]}")
+ 	message_container.text(f"top4:{smis_li[3]}")
+	message_container.text(f"top5:{smis_li[4]}")
 	return
 
 if __name__ == '__main__':
