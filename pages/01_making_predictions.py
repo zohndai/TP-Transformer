9d8255bfdefabfce2b028be0daa5c7ef89d6e19d
zohndai <zohndai@gmail.com>
2024-08-27 17:22:54 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -198,11 +198,11 @@ def main():
 	smis_li=["".join(dp_smi.split(" ")) for dp_smi in dp_smis[0]]
 	message_container = st.empty()
 	message_container.text(f"Prediction result:
-  top1:{smis_li[0]};\
-  top2:{smis_li[1]};\
-  top3:{smis_li[2]};\
-  top4:{smis_li[3]};\
-  top5:{smis_li[4]}"
+  top1:{smis_li[0]};/
+  top2:{smis_li[1]};/
+  top3:{smis_li[2]};/
+  top4:{smis_li[3]};/
+  top5:{smis_li[4]}"/
 			      )
 	return
 
