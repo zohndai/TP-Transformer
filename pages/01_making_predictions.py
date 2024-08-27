9a399dc1c00617f130fe1d34467e080444e77025
zohndai <zohndai@gmail.com>
2024-08-27 08:49:35 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -21,7 +21,7 @@ select=st.selectbox('What ROSs?', ('OH radical', 'SO4- radical', "Unkown"))
 st.write('You selected:', select)
 #select = st.radio("Please specify the property or activity you want to predict", ('OH radical', 'SO4- radical', 'Koc', 'Solubility','pKd','pIC50','CCSM_H','CCSM_Na', 'Lipo','FreeSolv' ))
 st.subheader('Please input the precursors of the ROSs')
-st.text_input("Please offer Chemical name, CAS number, or SMILES of precursors, e.g.'OO.[Fe+2]' for the fenton reagent H2O2/Fe2+ ", "OO.[Fe+2]")
+st.text_input("Please offer the SMILES of precursors, e.g.'OO.[Fe+2]' for the fenton reagent H2O2/Fe2+ ", "OO.[Fe+2]")
 
 st.subheader("Please select the method for extertal energy input for the ROSs generation", "UV")
 st.selectbox("what method?",("UV", "Heat", "Visible light", "Micro wave", "Electricity", "Ultrasound", "Sunlight"))
