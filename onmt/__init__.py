e930ad63424bbe44bbdda4f1b2db6c5c3a5b84ab
zohndai <zohndai@gmail.com>
2024-08-27 14:03:41 +0800

    Update __init__.py

--- onmt/__init__.py
+++ onmt/__init__.py
@@ -14,7 +14,6 @@ import onmt.utils.optimizers
 #sys.modules["onmt.Optim"] = onmt.utils.optimizers
 
 # For Flake
-__all__ = [onmt.inputters, onmt.encoders, onmt.decoders, onmt.models,
-           onmt.utils, onmt.modules, "Trainer"]
+#__all__ = [onmt.inputters, onmt.encoders, onmt.decoders, onmt.models, onmt.utils, onmt.modules, "Trainer"]
 
 __version__ = "1.0.0"
