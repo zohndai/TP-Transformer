b51a2ea8ebaad958467318cb68aff59b8f04ca20
zohndai <zohndai@gmail.com>
2024-08-27 09:53:02 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -61,33 +61,3 @@ if select=='OH radical':
 		xx = []
 	if "click2" not in st.session_state:
 		st.session_state.click2=False 
-	if col2.button('Get local interpretation') or st.session_state.click2:
-		st.session_state['click2']=True
-		#st.success(f'The predicted log second-rate constants toward OH radical is {st.session_state.output}')
-		explainer = shap.TreeExplainer(model_OH)
-		shap_value_train = explainer(x_input)
-		plot = shap.plots.waterfall(shap_value_train[0], show=False)
-		plt.xticks(fontsize= 18)
-		plt.yticks( fontsize = 18)
-		plt.tight_layout()
-		plt.savefig('h.png')
-		_, col31, _= st.columns([1,1,1])
-		col31.image('h.png')
-		#st.session_state.shaplot = 'h.png'
-	if "click3" not in st.session_state:
-		st.session_state.click3=False 
-	if col3.button('Get atom group') or st.session_state.click3:
-		st.session_state['click3']=True
-		ms1 = Chem.MolFromSmiles(smile)
-		fp1= AllChem.GetHashedMorganFingerprint(ms1, 0, 3764)
-		#st.success(f'The predicted log second-rate constants toward OH radical is {st.session_state.output}')
-		_, col31, _= st.columns([1,1,1])
-		#col31.image(st.session_state.shaplot)
-		st.success(f'The feature # containing atom groups are {list(fp1.GetNonzeroElements().keys())}')
-		st.session_state.sf =st.selectbox('Which Feature?', list(fp1.GetNonzeroElements().keys()))
-		bi = {}
-		fp_env = AllChem.GetMorganFingerprintAsBitVect(ms1, 0, 3764, bitInfo=bi)
-		y=Draw.DrawMorganBit(ms1, int(st.session_state.sf), bitInfo= bi)
-		_, col41, _= st.columns([1,2,1])
-		col41.image(y)
-		#st.session_state.atomgroup = 
