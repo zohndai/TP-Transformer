b308afc9c1e7289fd658a1560708533f5493529b
zohndai <zohndai@gmail.com>
2024-08-27 16:01:51 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -6,6 +6,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 import cirpy
 import torch
+import models
 from models import gdown_model
 #from rdkit.Chem import Draw
 #import matplotlib.pyplot as plt
