5c55d11f6b4ba9e4e5720346fe4d0cd353a7c897
zohndai <zohndai@gmail.com>
2024-08-27 16:28:37 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -8,6 +8,35 @@ import cirpy
 import torch
 #from rdkit.Chem import Draw
 #import matplotlib.pyplot as plt
+import os
+import gdown
+import time
+import steamlit as st
+def load_model(fd,model_name):
+    file_id = fd
+    model_path = model_name
+    download_url = f'https://drive.google.com/uc?id={file_id}'
+    gdown.download(download_url, model_path, quiet=True)
+#if col1.button('Get the prediction')
+def download():
+    name = '47700_step'
+    destination_dir = 'pt_models'
+    os.makedirs(destination_dir, exist_ok=True)
+    message_container = st.empty()
+    message_container.text("Downloading the models... Please wait.")
+    fd_dict = {'1IkvRvlpiLcETHrJahtLFuxiQtx0SBkdU':f'{name}_2024_0826',}
+    for fd in fd_dict.keys():
+        fd_file = fd
+        model_name = fd_dict[fd]
+        model_path = os.path.join(destination_dir, model_name)
+        load_model(fd_file, model_path)
+        time.sleep(4)
+        current_file_path = f'models/{model_name}'
+        new_file_path = f'models/{model_name}.pt'
+        if not os.path.exists(new_file_path):
+            os.rename(current_file_path, new_file_path)
+    message_container.tex("Model is ready!")
+  return new_file_path
 
 
 st.set_page_config(
@@ -63,7 +92,7 @@ with st.expander("Show how to get SMILES of chemicals"):
 	
 col1, col2, col3, col4= st.columns([2,2,1,1])
 if col1.button('Get the prediction'):
-	model_path = gdown_model.download()
+	model_path = download()
 def run():
 	col1, col2, col3, col4= st.columns([2,2,1,1])
 	if col1.button('Get the prediction'):
