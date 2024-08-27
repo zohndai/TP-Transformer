8ab98891978fe3208db5d47e9fd2897e065723e4
zohndai <zohndai@gmail.com>
2024-08-27 12:34:09 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -6,7 +6,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 import cirpy
 import torch
-from translate_code.translation import main
+from translation_code.translate import main
 #from rdkit.Chem import Draw
 #import matplotlib.pyplot as plt
 
