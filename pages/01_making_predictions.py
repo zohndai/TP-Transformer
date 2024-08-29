01cfe4a0802eb0af8bf4eca6985c8043e5594282
zohndai <zohndai@gmail.com>
2024-08-29 10:20:49 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -186,7 +186,7 @@ if True:
 		file.write(input)
 	
 	if col1.button('Get the prediction'):
-		if 'has_snow' ont in st.ssesion_state:
+		if 'has_snow' not in st.ssesion_state:
 		st.snow()
 		st.session_sate["has_snow"] = True
 		model_path = download()
