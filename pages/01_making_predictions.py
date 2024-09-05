3ad3a3d8cc806b176470ae77347daaa520003d45
zohndai <zohndai@gmail.com>
2024-09-05 13:43:07 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -180,7 +180,7 @@ def run():
 	
 	if col1.button('Get the prediction'):
 		if all([not(prec), not(ros_smi)]):
-			st.warning("At least one of 'ROSs and precursors' should be given, please check your input again")
+			st.warning("At least one of 'ROSs' and 'precursors' should be given, please check your input again")
 			st.stop()
 		model_path = download()
 		message_container = st.empty()
