ab59505924b7ed7f30052cd9ee915ca38253c29e
zohndai <zohndai@gmail.com>
2024-09-05 20:50:36 +0800

    Update Introduction_to_DP-Transformer.py

--- Introduction_to_DP-Transformer.py
+++ Introduction_to_DP-Transformer.py
@@ -51,10 +51,10 @@ if 'visitor_count' not in st.session_state:
 if 'session_initialized' not in st.session_state:
 	st.session_state.session_initialized = True
 st.session_state.visitor_count += 1
-visi.num += 1
+visitor.num += 1
 st.metric(label='👁️ Page Views', value=visi_num)
 st.write(f'Visitor Number: {st.session_state.visitor_count}')
-visi.to_csv("visi_num.txt", index=False)
+visitor.to_csv("visi_num.txt", index=False)
 #with open("visi_num.txt", 'w') as f:
 #	f.write(str(visi_num))
 #	f.close()
