d7ed436022c267a656bb71a0ebf1aa136b5eb6c7
zohndai <zohndai@gmail.com>
2024-09-05 14:54:53 +0800

    Update Introduction_to_DP-Transformer.py

--- Introduction_to_DP-Transformer.py
+++ Introduction_to_DP-Transformer.py
@@ -14,7 +14,7 @@ TEXT1 = """
         <body style='text-align: justify; color: black;'>
         <p> DP-Transformer platform is backend by advanced machine learning models to service users for predicting the degradation products of aqueous organic pollutants in chemical oxidation processes. 
 		DP-Transformer can now predict the degradation products and degradation pathways of organic pollutants. DP-Tramsformer uses SMILES to represent chemicals.   
-        </p>DP-Transformer is based on a similar model architecture as the Molecular Transformer but is distinguished by the utilized dataset. The well-trained DP-Transformer accepts pollutant SMILES, oxidative species,
+        </p>DP-Transformer is based on a Transformer architecture. The well-trained DP-Transformer accepts pollutant SMILES, oxidative species,
 	and reaction conditions (e.g., pH) as inputs and outputs the SMILES of the degradation products. This model is capable of predicting not only the degradation intermediates but also the complete degradation pathways. 
  The prediction of degradation pathways is accomplished through an iterative process, where a predicted degradation product made by DP-Transformer is used as input for subsequent prediction. This process continues until the 
  model predicts CO2 or when the predicted chemicals remain unchanged (i.e., non-degradable), indicating the formation of the final degradation products (Figure 1). 
@@ -22,21 +22,7 @@ TEXT1 = """
 	  </p>
         </body>         
         """
-GA_TRACKING_ID = "G-6MJ8FDZ7GH"
 
-GA_TRACKING_CODE = f"""
-<!-- Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={GA_TRACKING_ID}"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){{dataLayer.push(arguments);}}
-gtag('js', new Date());
-
-gtag('config', '{GA_TRACKING_ID}');
-</script>
-"""
-
-components.html(GA_TRACKING_CODE)
 if "show_animation" not in st.session_state:
     st.session_state.show_animation = True
 st.header('Welcome to DP-Transformer!')
